7941f840cb1cb59d1fce668a0b15f5d35afe2d4f
jramnesiac <jratrainbows@gmail.com>
2023-03-09 23:45:04 +0700

    Wrap all

--- js/index.js
+++ js/index.js
@@ -73,3 +73,47 @@
         repeater();
       });
 
+
+      function validate(){
+        var name = document.getElementById("name").value;
+        var subject = document.getElementById("alamat").value;
+        var phone = document.getElementById("nohp").value;
+        var email = document.getElementById("email").value;
+        var message = document.getElementById("message").value;
+        var error_message = document.getElementById("error_message");
+        
+        error_message.style.padding = "10px";
+        
+        var text;
+        if(name.length < 5){
+          text = "Masukan Nama duluu";
+          error_message.innerHTML = text;
+          return false;
+        }
+        if(subject.length < 10){
+          text = "Tuliskan Alamat yg benar";
+          error_message.innerHTML = text;
+          return false;
+        }
+        if(isNaN(phone) || phone.length != 10){
+          text = "masukin no HP yaa";
+          error_message.innerHTML = text;
+          return false;
+        }
+        if(email.indexOf("@") == -1 || email.length < 6){
+          text = "Email jangan lupaaa";
+          error_message.innerHTML = text;
+          return false;
+        }
+        if(message.length <= 140){
+          text = "Jangaan kepanjangan harus 140 kata";
+          error_message.innerHTML = text;
+          return false;
+        }
+        alert("Form Submitted Successfully!");
+        return true;
+      }
+
+  
+
+   
